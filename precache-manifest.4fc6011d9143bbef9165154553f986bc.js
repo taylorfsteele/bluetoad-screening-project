20b5b61b1d047e5319d1a0bd8783f2df40d1a973
taylorfsteele <taylor.f.steele@gmail.com>
2020-07-21 19:03:56 -0400

    Updates

---
+++ precache-manifest.4fc6011d9143bbef9165154553f986bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74c911939920cd353fbd9a689f245f81",
+    "url": "/bluetoad-screening-project/index.html"
+  },
+  {
+    "revision": "31f6449e3e12d8afdea1",
+    "url": "/bluetoad-screening-project/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ab8d9651d4c98176b77c",
+    "url": "/bluetoad-screening-project/static/js/2.ccc4a638.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/bluetoad-screening-project/static/js/2.ccc4a638.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31f6449e3e12d8afdea1",
+    "url": "/bluetoad-screening-project/static/js/main.bb20589d.chunk.js"
+  },
+  {
+    "revision": "a16fbc141f4dbcae9c11",
+    "url": "/bluetoad-screening-project/static/js/runtime-main.df6265bc.js"
+  }
+]);
